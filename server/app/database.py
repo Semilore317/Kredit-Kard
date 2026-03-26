15ad1151ebb6a598da58223ba36b7204df033fa4
Semilore317 <abraham.o.bankole@gmail.com>
2026-03-26 20:47:14 +0100

    chore: fixed typo

--- server/app/database.py
+++ server/app/database.py
@@ -4,11 +4,7 @@ from app.config import get_settings
 
 settings = get_settings()
 
-db_url = settings.database_url
-if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
-
-engine = create_engine(db_url)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
